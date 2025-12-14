807d63d02fc4e9a46815e7059f2ac771a65b2d9d
biloliddin <zaynobiddinovofficel@gmail.com>
2025-12-13 21:11:40 -1100

    Innitial commit

---
+++ html-elements.js
@@ -0,0 +1,9 @@
+const elHands = document.querySelectorAll(".js-hand");
+const elUser = document.querySelector("#user");
+const elRobot = document.querySelector("#robot");
+const elPlayAgainButton = document.querySelector("#PlayAgainButton");
+const elGameZone = document.querySelector("#gameZone");
+const elResultZone = document.querySelector("#resultZone");
+
+
+export { elHands, elUser, elRobot, elPlayAgainButton, elGameZone, elResultZone };
---
+++ js/app.js
@@ -0,0 +1,61 @@
+import { elGameZone, elHands, elPlayAgainButton, elResultZone, elRobot, elUser } from "../html-elements.js";
+
+elHands.forEach((elHand)=>{
+ elHand.addEventListener("click", (evt)=>{
+    console.log(evt.target.alt);
+  });
+});
+
+function robotChoose() {
+    const hands = ["paper", "scissors", "rock"];
+    const randomIndex = Math.trunc(Math.random() * hands.length);
+    return hands[randomIndex];
+}
+
+function swapZone(Boolean) {
+ if(Boolean) {
+    elGameZone.style.display = "none";
+    elResultZone.style.display = "flex";
+ } else {
+    elGameZone.style.display = "flex";
+    elResultZone.style.display = "none";
+ }
+}
+
+function checkWinner (user, robot) {
+    if (user === robot){
+      return "TIE"
+    } else if (user === "paper" && robot === "scissors"){
+         return "ROBOT";
+    } else if (user === "scissors" && robot === "rock"){
+         return "ROBOT";
+    } else if (user === "rock" && robot === "paper"){
+        return "ROBOT";
+    } else {
+        return "USER";
+    }
+}
+
+elHands.forEach((elHand) => {
+    elHand.addEventListener("click", (evt) => {
+      swapZone(true);
+
+      const user = evt.target.alt;
+      const robot = robotChoose();
+      
+      elUser.src = evt.target.src;
+      elRobot.src = "./img/handload.svg"
+
+      setTimeout(() => {
+        elRobot.src = `./img/${robot}.svg`
+        const winner = checkWinner(user, robot);
+       alert(winner)
+      }, 1000);
+    });
+});
+
+
+// Play Again Game
+elPlayAgainButton.addEventListener("click", () => {
+swapZone(false);
+});
